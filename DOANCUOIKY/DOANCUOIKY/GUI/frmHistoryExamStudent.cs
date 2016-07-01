c84ba5bbba726062ba523c4f158547be404bc5e6
chuongtnc <ntcchuong@gmail.com>
2016-07-01 19:19:59 +0700

    uppp

--- DOANCUOIKY/DOANCUOIKY/GUI/frmHistoryExamStudent.cs
+++ DOANCUOIKY/DOANCUOIKY/GUI/frmHistoryExamStudent.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DOANCUOIKY.BLL;
+using DOANCUOIKY.DTO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,9 +13,28 @@ namespace DOANCUOIKY.GUI
 {
     public partial class frmHistoryExamStudent : Form
     {
+        HistoryExamBLL historyExamBLL = new HistoryExamBLL();
+        DataGridViewRow gvRow = null;
         public frmHistoryExamStudent()
         {
             InitializeComponent();
+            loaddata();
         }
+        private void loaddata()
+        {
+            gvListExam.DataSource = historyExamBLL.LoadDataExamByParameters(UserInfoHolder.userInfo[0]);
+            gvListExam.Columns[0].Visible = false;
+        }
+
+        private void gvListExam_Click(object sender, EventArgs e)
+        {
+            if (gvListExam.CurrentRow != null)
+            {
+                gvRow = gvListExam.CurrentRow;
+                gvListQuestion.DataSource = historyExamBLL.LoadDataExamLineByParameters(gvRow.Cells[0].Value.ToString());
+                gvListQuestion.Columns[0].Visible = false;
+            }
+        }
+
     }
 }
